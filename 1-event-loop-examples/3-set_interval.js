eb771c5ff9fe06321532a6a86d0957cda56ae3b6
AgusBelP <aguspiuzzi@gmail.com>
2023-02-10 15:38:58 -0300

    event loop

---
+++ 1-event-loop-examples/3-set_interval.js
@@ -0,0 +1,6 @@
+setInterval(() => {
+    console.log('hello world');
+}, 2000);
+console.log('I will run first')
+
+// process stays alive unless you kill the process with Control + C or there's some unexpected error
